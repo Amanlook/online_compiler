0f0892e23ddfe2484ab44f36bdcfbc688ce437d6
Aman Sharma <110001067+Amanlook@users.noreply.github.com>
2026-06-04 22:57:43 +0530

    Change color codes in docker-setup.sh
    Updated color codes for output in docker-setup.sh.

--- docker-setup.sh
+++ docker-setup.sh
@@ -6,11 +6,11 @@
 set -e
 
 # Colors for output
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-NC='\033[0m' # No Color
+RED='\033[0;32m'
+GREEN='\033[0;33m'
+YELLOW='\033[1;34m'
+BLUE='\033[0;35m'
+NC='\036[0m' # No Color
 
 # Function to print colored output
 print_status() {
